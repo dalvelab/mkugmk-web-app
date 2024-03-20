f9bf201092d1f0d0d80093e77f08fc2e4759bc55
Vladislav Bragin <dalvelab@gmail.com>
2024-03-20 13:36:48 +0500

    feat(server): add entityService to welcome-page controller and edit response for exhibition-center controller

--- apps/server/src/api/exhibition-center/controllers/exhibition-center.js
+++ apps/server/src/api/exhibition-center/controllers/exhibition-center.js
@@ -10,11 +10,11 @@ module.exports = createCoreController('api::exhibition-center.exhibition-center'
   async find(ctx) {
     const locale = ctx.query.locale || 'all';
 
-    const data = await strapi.entityService.findMany('api::exhibition-center.exhibition-center', {
+    const response = await strapi.entityService.findMany('api::exhibition-center.exhibition-center', {
       populate: ['gallery', 'banner', 'youtube_gallery', 'working_time'],
       locale
     });
 
-    return data;
+    return { data: response };
   }
 }));
--- apps/server/src/api/welcome-page/controllers/welcome-page.js
+++ apps/server/src/api/welcome-page/controllers/welcome-page.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::welcome-page.welcome-page');
+module.exports = createCoreController('api::welcome-page.welcome-page', ({strapi}) => ({
+  async find(ctx) {
+    const locale = ctx.query.locale || 'all';
+
+    const response = await strapi.entityService.findMany('api::welcome-page.welcome-page', {
+      populate: ['gallery', 'banner', 'youtube_gallery'],
+      locale
+    });
+
+    return { data: response };
+  }
+}));
