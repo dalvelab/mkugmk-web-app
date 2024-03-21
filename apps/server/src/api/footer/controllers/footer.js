4f5b0661823294794bff85a7f79c21849c791879
Vladislav Bragin <dalvelab@gmail.com>
2024-03-21 21:38:15 +0500

    feat(server): update footer controller for entityService

--- apps/server/src/api/footer/controllers/footer.js
+++ apps/server/src/api/footer/controllers/footer.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::footer.footer');
+module.exports = createCoreController('api::footer.footer', ({strapi}) => ({
+  async find(ctx) {
+    const locale = ctx.query.locale || 'all';
+
+    const response = await strapi.entityService.findMany('api::footer.footer', {
+      populate: ['contacts', 'pages', 'socials', 'working_time'],
+      locale
+    });
+
+    return { data: response };
+  }
+}));
