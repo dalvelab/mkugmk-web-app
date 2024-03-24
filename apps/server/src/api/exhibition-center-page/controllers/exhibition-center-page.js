6665f1ea5a894ab255d617b9ebc25b5c5fc3ae28
Vladislav Bragin <dalvelab@gmail.com>
2024-03-24 18:53:45 +0500

    feat(server): add entityService to exhition-center-page find controller

--- apps/server/src/api/exhibition-center-page/controllers/exhibition-center-page.js
+++ apps/server/src/api/exhibition-center-page/controllers/exhibition-center-page.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::exhibition-center-page.exhibition-center-page');
+module.exports = createCoreController('api::exhibition-center-page.exhibition-center-page', ({strapi}) => ({
+  async find(ctx) {
+    const locale = ctx.query.locale || 'all';
+
+    const response = await strapi.entityService.findMany('api::exhibition-center-page.exhibition-center-page', {
+      populate: ['exhibition_centers', 'exhibition_centers.banner', 'exhibition_centers.working_time'],
+      locale
+    });
+
+    return { data: response };
+  }
+}));
