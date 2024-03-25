3ec372071492fdfd74926a661d4538d6893882af
Vladislav Bragin <dalvelab@gmail.com>
2024-03-25 11:46:20 +0500

    feat(server): add entityService to partners controller

--- apps/server/src/api/partner/controllers/partner.js
+++ apps/server/src/api/partner/controllers/partner.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::partner.partner');
+module.exports = createCoreController('api::partner.partner', ({strapi}) => ({
+  async find(ctx) {
+    const locale = ctx.query.locale || 'all';
+
+    const response = await strapi.entityService.findMany('api::partner.partner', {
+      populate: ['contacts', 'image'],
+      locale
+    });
+
+    return { data: response };
+  }
+}));
