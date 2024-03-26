8c12fb5f7b77ffe06a2e59ca48e42d7a998d9908
Vladislav Bragin <dalvelab@gmail.com>
2024-03-26 13:01:07 +0500

    feat(server): change rus names for eng in week days component

--- apps/server/types/generated/components.d.ts
+++ apps/server/types/generated/components.d.ts
@@ -68,15 +68,7 @@ export interface SharedWorkingTime extends Schema.Component {
     value: Attribute.String & Attribute.Required;
     opened: Attribute.Boolean & Attribute.Required & Attribute.DefaultTo<true>;
     day: Attribute.Enumeration<
-      [
-        '\u043F\u043E\u043D\u0435\u0434\u0435\u043B\u044C\u043D\u0438\u043A',
-        '\u0432\u0442\u043E\u0440\u043D\u0438\u043A',
-        '\u0441\u0440\u0435\u0434\u0430',
-        '\u0447\u0435\u0442\u0432\u0435\u0440\u0433',
-        '\u043F\u044F\u0442\u043D\u0438\u0446\u0430',
-        '\u0441\u0443\u0431\u0431\u043E\u0442\u0430',
-        '\u0432\u043E\u0441\u043A\u0440\u0435\u0441\u0435\u043D\u044C\u0435'
-      ]
+      ['mon', 'tue', 'wed', 'thurs', 'fri', 'sat', 'sun']
     >;
   };
 }
