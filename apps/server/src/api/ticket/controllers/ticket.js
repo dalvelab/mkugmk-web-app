23abc8e47a15f6afa53194e8344907f4eec16d6c
Vladislav Bragin <dalvelab@gmail.com>
2024-04-01 21:17:32 +0500

    feat(web): add entityService to tickets controller

--- apps/server/src/api/ticket/controllers/ticket.js
+++ apps/server/src/api/ticket/controllers/ticket.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::ticket.ticket');
+module.exports = createCoreController('api::ticket.ticket', ({strapi}) => ({
+  async find(ctx) {
+    const locale = ctx.query.locale || 'all';
+
+    const response = await strapi.entityService.findMany('api::ticket.ticket', {
+      populate: ['exhibition_centers', 'exhibition_centers.banner'],
+      locale
+    });
+
+    return { data: response };
+  }
+}));
