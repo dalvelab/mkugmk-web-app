f089a5820ad2280e68ad117d258d102925191676
Vladislav Bragin <dalvelab@gmail.com>
2024-06-06 11:19:24 +0500

    feat(web): add updated privacy policy

--- apps/web/src/features/OrderCall/OrderCall.tsx
+++ apps/web/src/features/OrderCall/OrderCall.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import { useTranslations } from "next-intl";
 
-import { 
+import {
   chakra,
   Button,
   Modal,
@@ -11,7 +11,7 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
-  useDisclosure, 
+  useDisclosure,
   Heading,
   Flex,
   FormLabel,
@@ -20,7 +20,7 @@ import {
   Textarea,
   FormErrorMessage,
   useToast,
-  ButtonProps
+  ButtonProps,
 } from "@chakra-ui/react";
 import { z } from "zod";
 import { isNotVoid } from "@/shared";
@@ -31,38 +31,40 @@ const FormSchema = z.object({
   name: z.string().min(2, "Имя должно содержать минимум 2 символа"),
   phone: z.string().min(1, "Телефон должен быть указан"),
   message: z.string().min(1, "Сообщение не может быть пустым"),
-})
+});
 
 const initalForm = {
   name: "",
   phone: "",
   message: "",
-}
+};
 
 interface OrderCallProps {
   buttonStyles?: ButtonProps["__css"];
 }
 
-export const OrderCall: React.FC<OrderCallProps> = ({buttonStyles}) => {
+export const OrderCall: React.FC<OrderCallProps> = ({ buttonStyles }) => {
   const { locale } = useRouter();
   const toast = useToast();
 
-  const t = useTranslations('Footer');
+  const t = useTranslations("Footer");
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const [form, setForm] = useState(initalForm);
-  const [errors, setErrors] = useState<z.ZodError<z.infer<typeof FormSchema>> | null>(null);
+  const [errors, setErrors] = useState<z.ZodError<
+    z.infer<typeof FormSchema>
+  > | null>(null);
 
   const mutation = useMutation({
     mutationFn: sendEmailRequest,
     onSuccess: () => {
       toast({
-        title: 'Успешно!',
+        title: "Успешно!",
         description: "С вами свяжутся в ближайшее время",
-        status: 'success',
+        status: "success",
         duration: 2500,
-        position: 'top-right',
+        position: "top-right",
         isClosable: true,
       });
       setForm(initalForm);
@@ -72,13 +74,13 @@ export const OrderCall: React.FC<OrderCallProps> = ({buttonStyles}) => {
       setForm(initalForm);
 
       toast({
-        title: 'Произошла ошибка.',
+        title: "Произошла ошибка.",
         description: "Попробуйте позже",
-        status: 'error',
+        status: "error",
         duration: 2500,
-        position: 'top-right',
+        position: "top-right",
         isClosable: true,
-      })
+      });
     },
   });
 
@@ -91,7 +93,9 @@ export const OrderCall: React.FC<OrderCallProps> = ({buttonStyles}) => {
       return;
     }
 
-    mutation.mutate({message: `Заказ звонка с сайта. \n Имя: ${form.name}\n Телефон: ${form.phone}\n Сообщение: ${form.message}`})
+    mutation.mutate({
+      message: `Заказ звонка с сайта. \n Имя: ${form.name}\n Телефон: ${form.phone}\n Сообщение: ${form.message}`,
+    });
   }
 
   function onCloseModal() {
@@ -100,20 +104,24 @@ export const OrderCall: React.FC<OrderCallProps> = ({buttonStyles}) => {
     setForm(initalForm);
   }
 
-  const nameError = errors?.issues.find((issue) => issue.path.includes('name'));
-  const phoneError = errors?.issues.find((issue) => issue.path.includes('phone'));
-  const messageError = errors?.issues.find((issue) => issue.path.includes('message'));
+  const nameError = errors?.issues.find((issue) => issue.path.includes("name"));
+  const phoneError = errors?.issues.find((issue) =>
+    issue.path.includes("phone")
+  );
+  const messageError = errors?.issues.find((issue) =>
+    issue.path.includes("message")
+  );
 
   return (
     <>
       <Button
         onClick={onOpen}
-        variant='link'
+        variant="link"
         fontWeight="regular"
         __css={buttonStyles}
-        _hover={{textDecor: "underline"}}
+        _hover={{ textDecor: "underline" }}
       >
-        {t('order_call')}
+        {t("order_call")}
       </Button>
       <Modal
         size={["full", "lg", "lg", "lg", "lg"]}
@@ -124,23 +132,31 @@ export const OrderCall: React.FC<OrderCallProps> = ({buttonStyles}) => {
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>
-            <Heading color="brand.black" pt={4} as="h6" fontSize="3xl" fontWeight="medium">
-              {t('order_call')}
+            <Heading
+              color="brand.black"
+              pt={4}
+              as="h6"
+              fontSize="3xl"
+              fontWeight="medium"
+            >
+              {t("order_call")}
             </Heading>
-            <ModalCloseButton />          
+            <ModalCloseButton />
           </ModalHeader>
           <ModalBody pb={4}>
             <Flex flexDir="column" gap={5}>
               <FormControl isRequired isInvalid={isNotVoid(nameError)}>
-                <FormLabel>{t('order_call_modal_form_name')}</FormLabel>
+                <FormLabel>{t("order_call_modal_form_name")}</FormLabel>
                 <Input
                   isInvalid={isNotVoid(nameError)}
-                  placeholder={t('order_call_modal_form_name')}
+                  placeholder={t("order_call_modal_form_name")}
                   value={form.name}
-                  onChange={(event) => setForm({
-                    ...form,
-                    name: event.target.value
-                  })}
+                  onChange={(event) =>
+                    setForm({
+                      ...form,
+                      name: event.target.value,
+                    })
+                  }
                   onFocus={() => setErrors(null)}
                 />
                 {isNotVoid(nameError) && (
@@ -148,16 +164,18 @@ export const OrderCall: React.FC<OrderCallProps> = ({buttonStyles}) => {
                 )}
               </FormControl>
               <FormControl isRequired isInvalid={isNotVoid(phoneError)}>
-                <FormLabel>{t('order_call_modal_form_phone')}</FormLabel>
+                <FormLabel>{t("order_call_modal_form_phone")}</FormLabel>
                 <Input
                   isInvalid={isNotVoid(phoneError)}
-                  placeholder={t('order_call_modal_form_phone')}
+                  placeholder={t("order_call_modal_form_phone")}
                   value={form.phone}
                   maxLength={12}
-                  onChange={(event) => setForm({
-                    ...form,
-                    phone: event.target.value.replace(/[^\d.+]+/g, "")
-                  })}
+                  onChange={(event) =>
+                    setForm({
+                      ...form,
+                      phone: event.target.value.replace(/[^\d.+]+/g, ""),
+                    })
+                  }
                   onFocus={() => setErrors(null)}
                 />
                 {isNotVoid(phoneError) && (
@@ -165,15 +183,17 @@ export const OrderCall: React.FC<OrderCallProps> = ({buttonStyles}) => {
                 )}
               </FormControl>
               <FormControl isRequired isInvalid={isNotVoid(messageError)}>
-                <FormLabel>{t('order_call_modal_form_message')}</FormLabel>
+                <FormLabel>{t("order_call_modal_form_message")}</FormLabel>
                 <Textarea
                   isInvalid={isNotVoid(messageError)}
-                  placeholder={t('order_call_modal_form_message')}
+                  placeholder={t("order_call_modal_form_message")}
                   value={form.message}
-                  onChange={(event) => setForm({
-                    ...form,
-                    message: event.target.value
-                  })}
+                  onChange={(event) =>
+                    setForm({
+                      ...form,
+                      message: event.target.value,
+                    })
+                  }
                   onFocus={() => setErrors(null)}
                 />
                 {isNotVoid(messageError) && (
@@ -183,15 +203,32 @@ export const OrderCall: React.FC<OrderCallProps> = ({buttonStyles}) => {
               <Button
                 bgColor="brand.black"
                 color="white"
-                _hover={{bgColor: "brand.black"}}
-                _focus={{bgColor: "brand.black"}}
+                _hover={{ bgColor: "brand.black" }}
+                _focus={{ bgColor: "brand.black" }}
                 onClick={onClick}
               >
-                {t('order_call')}
+                {t("order_call")}
               </Button>
-              {locale === 'ru' && (
+              {locale === "ru" && (
                 <chakra.span fontSize="xs" color="brand.gray">
-                  Нажимая кнопку вы соглашаетесь с <chakra.a textDecor="underline" href="/">политикой конфиденциальности</chakra.a>
+                  Нажимая на кнопку вы соглашаетесь с{" "}
+                  <chakra.a
+                    textDecor="underline"
+                    href="/uploads/museum_privacy_policy_747e25d95e.pdf"
+                    target="_blank"
+                    _hover={{ color: "brand.green" }}
+                  >
+                    Политикой защиты и обработки персональных данных в ЧУК
+                    «Музейный комплекс»
+                  </chakra.a>{" "}
+                  и подтверждаете, что ознакомлены и согласны с{" "}
+                  <chakra.a
+                    textDecor="underline"
+                    href="/visitors/rules"
+                    _hover={{ color: "brand.green" }}
+                  >
+                    Правилами посещения музейного комплекса
+                  </chakra.a>
                 </chakra.span>
               )}
             </Flex>
@@ -199,5 +236,5 @@ export const OrderCall: React.FC<OrderCallProps> = ({buttonStyles}) => {
         </ModalContent>
       </Modal>
     </>
-  )
-}
+  );
+};
