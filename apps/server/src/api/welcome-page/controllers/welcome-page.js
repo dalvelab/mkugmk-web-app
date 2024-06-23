c38995c9778abf6a448e2aa8b8a87c7195e53b86
Vladislav Bragin <dalvelab@gmail.com>
2024-06-23 16:24:53 +0500

    feat(server): populate exhibition centers on welcome pages

--- apps/server/src/api/welcome-page/controllers/welcome-page.js
+++ apps/server/src/api/welcome-page/controllers/welcome-page.js
@@ -1,20 +1,32 @@
-'use strict';
+"use strict";
 
 /**
  * welcome-page controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::welcome-page.welcome-page', ({strapi}) => ({
-  async find(ctx) {
-    const locale = ctx.query.locale || 'all';
+module.exports = createCoreController(
+  "api::welcome-page.welcome-page",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const locale = ctx.query.locale || "all";
 
-    const response = await strapi.entityService.findMany('api::welcome-page.welcome-page', {
-      populate: ['gallery', 'banner', 'youtube_gallery', 'video_preview'],
-      locale
-    });
+      const response = await strapi.entityService.findMany(
+        "api::welcome-page.welcome-page",
+        {
+          populate: [
+            "gallery",
+            "banner",
+            "youtube_gallery",
+            "video_preview",
+            "exhibition_centers",
+          ],
+          locale,
+        }
+      );
 
-    return { data: response };
-  }
-}));
+      return { data: response };
+    },
+  })
+);
