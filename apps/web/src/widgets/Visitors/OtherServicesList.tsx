d3de04d3dc5e4a43e47cbe795cb8876dcac93b3d
Vladislav Bragin <dalvelab@gmail.com>
2024-07-22 12:20:20 +0500

    feat(web): make new UI for other services

--- apps/web/src/widgets/Visitors/OtherServicesList.tsx
+++ apps/web/src/widgets/Visitors/OtherServicesList.tsx
@@ -1,20 +1,4 @@
-import { useRef, useState } from "react";
-import { useTranslations } from "next-intl";
-
-import {
-  Badge,
-  chakra,
-  Container,
-  Flex,
-  Grid,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
+import { Badge, chakra, Container, Flex, Grid } from "@chakra-ui/react";
 
 import type { TicketsAndServicesPage } from "@/entities";
 import { isNotVoid, Markdown } from "@/shared";
@@ -26,33 +10,12 @@ interface OtherServicesListProps {
 export const OtherServicesList: React.FC<OtherServicesListProps> = ({
   other_services,
 }) => {
-  const t = useTranslations("Tickets_page");
-
-  const [activeId, setActiveId] = useState<number>(other_services[0].id);
-
-  const tableRef = useRef<HTMLDivElement | undefined>();
-
-  function onClick(id: number) {
-    setActiveId(id);
-
-    if (isNotVoid(tableRef.current)) {
-      window.scrollTo({
-        top: tableRef.current.offsetTop - 100,
-        behavior: "smooth",
-      });
-    }
-  }
-
-  const activeService = other_services.find(
-    (service) => service.id === activeId
-  );
-
   const bgColorMap: Record<string, string> = {
-    '0': 'blue',
-    '1': 'green',
-    '2': 'yellow',
-    '3': 'orange',
-  }
+    "0": "blue",
+    "1": "green",
+    "2": "yellow",
+    "3": "orange",
+  };
 
   return (
     <chakra.section pb={10}>
@@ -62,124 +25,44 @@ export const OtherServicesList: React.FC<OtherServicesListProps> = ({
           alignItems="flex-start"
           flexDir={["column", "column", "column", "row", "row"]}
         >
-          {/* <Flex
-            minW="340px"
-            maxW="390px"
-            w={["100%", "auto", "auto", "auto", "auto"]}
-            flexDir="column"
-            py={3}
-            px={5}
-            border="1px solid"
-            borderColor="brand.border"
-            borderRadius={8}
-            color="brand.black"
-            gap={3}
+          <Grid
+            templateColumns={[
+              "1fr",
+              "1fr",
+              "1fr 1fr",
+              "1fr 1fr",
+              "1fr 1fr 1fr",
+            ]}
+            gap={5}
           >
-            <chakra.span fontSize="xl" fontWeight="semibold">
-              {t("type_of_service")}
-            </chakra.span>
-            <Flex flexDir="column" gap={1}>
-              {other_services.map((button) => (
-                <chakra.button
-                  key={button.id}
-                  py={3}
-                  px={4}
-                  backgroundColor={button.id === activeId ? "#F4F4F5" : "white"}
-                  _hover={{ bgColor: "#F4F4F5" }}
-                  borderRadius={8}
-                  onClick={() => onClick(button.id)}
+            {other_services.map((service) => (
+              <Flex
+                key={service.id}
+                p={5}
+                flexDir="column"
+                border="1px solid"
+                borderColor="brand.border"
+                borderRadius={8}
+              >
+                <Badge
+                  fontSize="sm"
+                  colorScheme="green"
+                  px={2}
+                  alignSelf="flex-start"
                 >
-                  <Flex flexDir="column" alignItems="flex-start" gap={0.5}>
-                    <chakra.span whiteSpace="pre-wrap" textAlign="left">
-                      {button.name}
-                    </chakra.span>
-                  </Flex>
-                </chakra.button>
-              ))}
-            </Flex>
-          </Flex> */}
-          <Grid templateColumns="1fr 1fr 1fr" gap={5}>
-          {other_services.map((service, index) => (
-            <Flex
-            key={service.id}
-            p={5}
-            flexDir="column"
-            border="1px solid"
-            borderColor="brand.border"
-            borderRadius={8}
-            > 
-            <Badge colorScheme={bgColorMap[index]} px={2} alignSelf="flex-start">{service.name}</Badge>
-            <chakra.span mt={5} fontSize="2xl" fontWeight="medium">{service.value}</chakra.span>
-            <chakra.span mt={5} color="brand.gray" fontSize="sm">
-              <Markdown>{service.additional_text}</Markdown>
-            </chakra.span>
-            </Flex>
-          ))}
-          </Grid>
-          {/* @ts-ignore */}
-          {/* <TableContainer ref={tableRef} w="100%">
-            <Table border="1px solid" borderColor="brand.border">
-              <Thead>
-                <Tr>
-                  <Th
-                    w="50%"
-                    fontSize="xl"
-                    textTransform="none"
-                    color="brand.black"
-                    px={5}
-                    py={5}
-                    fontWeight="semibold"
-                  >
-                    {t("price")}
-                  </Th>
-                  <Th
-                    w="50%"
-                    fontSize="xl"
-                    textTransform="none"
-                    color="brand.black"
-                    px={5}
-                    py={5}
-                    border="1px solid"
-                    borderColor="brand.border"
-                    fontWeight="semibold"
-                  >
-                    {t("info")}
-                  </Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {isNotVoid(activeService) && (
-                  <Tr>
-                    <Td
-                      w="50%"
-                      minW="280px"
-                      px={5}
-                      border="1px solid"
-                      borderColor="brand.border"
-                      verticalAlign="top"
-                    >
-                      <Flex flexDir="column" gap={6} textAlign="justify">
-                        <Markdown>{activeService.value}</Markdown>
-                      </Flex>
-                    </Td>
-                    {isNotVoid(activeService.additional_text) && (
-                      <Td
-                        w="50%"
-                        minW="300px"
-                        px={5}
-                        border="1px solid"
-                        borderColor="brand.border"
-                        verticalAlign="top"
-                        textAlign="justify"
-                      >
-                        <Markdown>{activeService.additional_text}</Markdown>
-                      </Td>
-                    )}
-                  </Tr>
+                  {service.name}
+                </Badge>
+                <chakra.span mt={5} fontSize="lg" fontWeight="medium">
+                  {service.value}
+                </chakra.span>
+                {isNotVoid(service.additional_text) && (
+                  <chakra.span mt={5} color="brand.gray" fontSize="sm">
+                    <Markdown>{service.additional_text}</Markdown>
+                  </chakra.span>
                 )}
-              </Tbody>
-            </Table>
-          </TableContainer> */}
+              </Flex>
+            ))}
+          </Grid>
         </Flex>
       </Container>
     </chakra.section>
