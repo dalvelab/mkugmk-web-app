7c3106af0f1be7a3b7a105dea02f265e326b6df3
dalvelab <dalvelab@gmail.com>
2025-04-30 12:16:27 +0500

    update promo page

--- apps/web/src/pages/promo/telegram/index.tsx
+++ apps/web/src/pages/promo/telegram/index.tsx
@@ -1,32 +1,43 @@
 import { chakra, Spinner } from "@chakra-ui/react";
 import type { GetServerSideProps } from "next";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 2000;
 
 export default function Promo() {
+  const [countdown, setCountdown] = useState(TIMER / 1000);
+
   useEffect(() => {
     const timer = setTimeout(() => {
-      window.location.href = "https://t.me/mk_pyshma";
-    }, 1000);
+      setCountdown((prev) => prev - 1);
+      if (countdown === 1) {
+        clearTimeout(timer);
+        window.location.href = "https://t.me/mk_pyshma";
+      }
+    }, TIMER);
 
     return () => {
       clearTimeout(timer);
     };
-  }, []);
+  }, [countdown]);
 
   return (
-    <>
-      <chakra.section
-        pt={6}
-        pb={10}
-        display="flex"
-        h="80vh"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Spinner size="xl" />
-      </chakra.section>
-    </>
+    <chakra.section
+      pt={6}
+      pb={10}
+      display="flex"
+      h="80vh"
+      justifyContent="center"
+      alignItems="center"
+      flexDir="column"
+      gap={5}
+    >
+      <Spinner size="xl" />
+      <chakra.span fontSize="xl" textAlign="center">
+        Переход произойдет через: <br /> {countdown}
+      </chakra.span>
+    </chakra.section>
   );
 }
 
